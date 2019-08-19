68bf822fe0295df48201be1aafeefbb501e93ffa
Hanumeshjitta <39722076+Hanumeshjitta@users.noreply.github.com>
2019-08-19 20:03:09 +0530

    Update welcome.java

--- welcome.java
+++ welcome.java
@@ -2,6 +2,6 @@ class Welcome{
   public static void main(String[] args){
    System.out.println("Welcome to Devops Certification program");
    System.out.println("###Initial code is modified##");
-    System.out.println("### This code is entered an Git lab##");
+   System.out.println("### This code is entered an Git lab##");
   }
 }
